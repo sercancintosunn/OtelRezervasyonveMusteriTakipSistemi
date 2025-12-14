21724eaab34690f3f23f83042aada161d9db51ac
emru <emircncngz3@gmail.com>
2025-12-14 18:21:45 +0300

    OdalarFrame eklendi

---
+++ src/main/java/com/otel/controller/OdalarController.java
@@ -0,0 +1,10 @@
+package com.otel.controller;
+
+import com.otel.view.OdalarFrame;
+
+public class OdalarController {
+    private OdalarFrame view;
+    public OdalarController(OdalarFrame view) {
+        this.view = view;
+    }
+}
